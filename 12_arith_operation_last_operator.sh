93726e2d329cddf60e821ddaa0429401a04a10d6
Sathish <sathishkd28996@gmail.com>
2023-05-29 15:23:50 +0530

    Worked on 29th May

--- 12_arith_operation_last_operator.sh
+++ 12_arith_operation_last_operator.sh
@@ -2,6 +2,18 @@
 
 read -p "Enter the string: " str
 
-oper=${str:(-1):5}
+oper=${str:(-1)}
+len=${#str}
+last=$(( $len - 1 )) 
+str=${str:0:$last}
 
-echo "$oper"
+final=1
+
+while [ $str -gt 0 ];do
+    rem=$(( ${str} % 10 ))
+    final=$(( ${final} ${oper} ${rem} ))
+    str=$(( ${str} / 10 ))
+done
+
+echo $final
+    
